662fbfd282ed258ac11b6717328020bf3e0b459c
Nisorina <nisorina@mail.ru>
2023-02-07 15:55:58 +0300

    Task2

---
+++ app/src/main/java/ru/otus/nisorina/homework_01/Task2.kt
@@ -0,0 +1,30 @@
+package ru.otus.nisorina.homework_01
+/* Задание #2
+ - Реализуйте свой дата класс Person
+ - Добавьте в него следующие проперти name:String, surname:String, age:Int
+ - Реализуйте экстеншен функцию на список типизированный Person, которая сортирует список по возрасту в порядке убывания
+ - Реализуйте экстеншен функцию на список типизированный Person, которая сортирует список в алфавитном порядке по имени.
+   Если имена одинаковые, то учитывается фамилия */
+
+class Person(val surname: String, val name: String, val age: Int) {
+    override fun toString(): String {
+        return "$name $surname ($age)"
+    }
+}
+
+fun List<Person>.sortByAge() = sortedByDescending { it.age }
+fun List<Person>.sortByName() = sortedBy { it.name } .sortedBy { it.surname }
+
+fun main() {
+    val persons = listOf(
+        Person("Иванов", "Иван", 35),
+        Person("Сергеев", "Сергей", 21),
+        Person("Петров", "Петр", 38),
+        Person("Алехин", "Иван", 15),
+    )
+    println("По возрасту:" )
+    persons.sortByAge().forEach { println(it) }
+    println("\nПо имени:" )
+    persons.sortByName().forEach { println(it) }
+}
+
